3f37123b45329f300be22b70484c3b25ef3c63b9
Paloma <stpaloma23@gmail.com>
2021-04-14 01:27:04 -0400

    final zodiac website using JSON colections

--- scripts.js
+++ scripts.js
@@ -1,126 +1,152 @@
-let 
+// adding the json to the html
+let output = document.getElementById("collectionOfZodiacs");
+
 var jsonZodiacsDatabase = [
   {
     "sign":"Aries",
     "dates": "March 20 - April 19",
-    "element":"fire",
+    "element":"Fire",
     "planet": "Mars",
-    "symbol": "https://askastrology.com/wp-content/uploads/2019/12/Aries_icon-258x300.jpg"
+    "symbol": "images/aries.png",
+    "color": "#ff8c00"
   },
   {
     "sign":"Taurus",
     "dates": "April 19-May 20",
     "element":"Earth",
     "planet": "Venus",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Taurus_icon.jpg"
+    "symbol":"images/taurus.png",
+    "color": "#87a96b"
   },
   {
     "sign":"Gemini",
     "dates": "May 20 - June 21",
     "element":"Air",
     "planet": "Mercury",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Gemini_icon.jpg"
+    "symbol":"images/gemini.png",
+    "color": "#f2f3f4"
   },
   {
     "sign":"Cancer",
     "dates": "June 21 - July 22",
     "element":"Water",
     "planet": "the Moon",
-    "symbol": "https://askastrology.com/wp-content/uploads/2019/12/Cancer_icon.jpg"
+    "symbol": "images/cancer.png",
+    "color": "#87cefa"
   },
   {
     "sign":"Leo",
     "dates": "July 22 - August 22",
     "element":"Fire",
     "planet": "the Sun",
-    "symbol": "https://askastrology.com/wp-content/uploads/2019/12/Leo_icon.jpg"
+    "symbol": "images/leo.png",
+    "color": "#ff8c00"
   },
   {
     "sign":"Virgo",
     "dates": "August 22 - September 22",
     "element":"Earth",
     "planet": "Mercury",
-    "symbol": "https://askastrology.com/wp-content/uploads/2019/12/Virgo_icon.jpg"
+    "symbol": "images/virgo.png",
+    "color": "#87a96b"
   },
   {
     "sign":"Libra",
     "dates": "September 22 - October 23",
-    "element":"Wind",
+    "element":"Air",
     "planet": "Venus",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Libra_icon.jpg"
+    "symbol":"images/libra.png",
+    "color": "#f2f3f4"
   },
   {
     "sign":"Scorpio",
     "dates": "October 23 - November 22",
     "element":"Water",
     "planet": "Pluto",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Scorpio_icon.jpg"
+    "symbol":"images/scorpio.png",
+    "color": "#87cefa"
   },
   {
     "sign":"Sagittarius",
     "dates": "November 22 - December 21",
     "element":"Fire",
     "planet": "Jupiter",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Sagittarius_icon.jpg"
+    "symbol":"images/sag.png",
+    "color": "#ff8c00"
   },
   {
     "sign":"Capricorn",
     "dates": "December 21 - January 20",
     "element":"Earth",
     "planet": "Saturn",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Capricorn_icon.jpg"
+    "symbol":"images/cap.png",
+    "color": "#87a96b"
   },
   {
     "sign":"Aquarius",
     "dates": "January 19 - February 18",
-    "element":"Wind",
+    "element":"Air",
     "planet": "Uranus",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Aquarius_icon.jpg"
+    "symbol":"images/aqua.png",
+    "color": "#f2f3f4"
   },
   {
     "sign":"Pisces",
     "dates": "February 18 - March 20",
     "element":"Water",
     "planet": "Neptune",
-    "symbol":"https://askastrology.com/wp-content/uploads/2019/12/Pisces_icon.jpg"
+    "symbol":"images/pisces.png",
+    "color": "#87cefa"
   }
 ];
 
-document.addEventListener('DOMContentLoaded', function(){
-  var zodiacOutput = document.getElementById("zodiacOutput");
-  var outputPara = document.getElementById("zodiacOutputPara");
-  for (var i=0; i<jsonZodiacsDatabase.length; i++){
-    displayZodiacs(jsonZodiacsDatabase[i]);
+//reading from the JSON colelction and sending each index to be added to html website
+for (var i=0; i<jsonZodiacsDatabase.length; i++){
+  createZodiacs(jsonZodiacsDatabase[i]);
+}
+
+// this function takes the json collection and adds the elemnts to html
+function createZodiacs(partOfDatabase){
+  let newDiv = document.createElement("DIV"); //creating html tag
+  newDiv.classList.add("zodiacSign"); // creating css tag
+  //setting the color of each box, color is based of element
+  if(partOfDatabase["element"] == "Fire"){
+    newDiv.style.backgroundColor = partOfDatabase["color"];
   }
-});
+  else if (partOfDatabase["element"] == "Earth") {
+    newDiv.style.backgroundColor = partOfDatabase["color"] ;
+  }
+  else if (partOfDatabase["element"] == "Water") {
+    newDiv.style.backgroundColor = partOfDatabase["color"];
+  }
+  else if (partOfDatabase["element"] == "Air") {
+    newDiv.style.backgroundColor = partOfDatabase["color"];
+  }
 
-function displayZodiacs(partOfDatabase){
-  let newDiv = document.createElement("DIV");
-  newDiv.classList.add("container")
-
-
-  let zodiacName  = document.createElement("H3");
-  zodiacName.classList.add("name");
-  zodiacName.innerText = partOfDatabase["sign"];
-  newDiv.appendChild(zodiacName);
+  let zodiacName  = document.createElement("H3"); //create html tag
+  zodiacName.innerText = partOfDatabase["sign"]; //find the data in the json and write it to the html
+  zodiacName.classList.add("zName"); //css tag
+  newDiv.appendChild(zodiacName); //adding the content to the box
 
   let dates = document.createElement("H4");
+  dates.innerText = "Dates: "+partOfDatabase["dates"];
   dates.classList.add("dateRange");
-  dates.innerText = partOfDatabase["dates"];
   newDiv.appendChild(dates);
 
   let elements = document.createElement("H4");
   elements.classList.add("elements");
-  elements.innerText = partOfDatabase["element"];
+  elements.innerText = "Element: " + partOfDatabase["element"];
   newDiv.appendChild(elements);
 
   let  planetRule= document.createElement("H4");
   planetRule.classList.add("rulingPlanet");
-  planetRule.innerText = partOfDatabase["planet"];
+  planetRule.innerText = "Planet: " + partOfDatabase["planet"];
   newDiv.appendChild(planetRule);
 
   let theImage = document.createElement("IMG");
   theImage.classList.add("zodiacSymbol");
   theImage.src = partOfDatabase["symbol"];
   newDiv.appendChild(theImage);
+
+  output.appendChild(newDiv); // adding the new data to the html 
 }
